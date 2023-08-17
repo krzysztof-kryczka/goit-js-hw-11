172d97fdd9862e0a504c8a6edcdde3af5f50f8e3
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-08-17 23:43:48 +0200

    add function photoCardTemplate and display images result

--- src/index.js
+++ src/index.js
@@ -23,20 +23,60 @@ async function onSubmitForm(e) {
 }
 
 function loadingImages(page, value) {
-  fetchImages(value, page)
-    .then(data => {
-      console.log(data);
-      if (data.totalHits === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
+  fetchImages(value, page).then(data => {
+    console.log(data);
+    const searchResults = data.hits;
+    if (data.totalHits === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
 
-      if (data.totalHits !== 0) {
-        Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      }
-    })
+    if (data.totalHits !== 0) {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      photoCardTemplate(searchResults);
+    }
+  });
+}
+
+function photoCardTemplate(searchResults) {
+  const markup = searchResults.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+    }
+  );
+  obj.gallery.insertAdjacentHTML('beforeend', markup.join(''));
 }
 
 obj.form.addEventListener('submit', onSubmitForm);
